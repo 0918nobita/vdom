f6866aa077065aec318431b49a0291d0ff34c041
0918nobita <nobita.0918@gmail.com>
2021-06-03 01:40:29 +0900

    diff/catch-error を移植した

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,51 @@
-import { h } from './hyperscript';
-import { ActionSet, View, makeApp } from './app';
-
-interface CounterState {
-    count: number;
+interface ComponentConstructor {
+    getDerivedStateFromError: null | ((error: Error) => any);
 }
 
-const initialState: CounterState = {
-    count: 0,
+interface Component {
+    constructor: ComponentConstructor;
+    setState(state: any): void;
+    componentDidCatch: null | ((error: Error) => void);
+    _dirty: any;
+    _pendingError: any;
+    _processingException: any;
+}
+
+interface VNode {
+    _component: null | Component;
+    _parent: any;
+}
+
+/** 最も近いエラー境界を見つけ、エラーを投げた上でそれを呼び出す */
+const _catchError = (error: Error, vnode: VNode) => {
+    for (; (vnode = vnode._parent); ) {
+        const component = vnode._component;
+
+        if (!component || component._processingException) continue;
+
+        try {
+            const ctor = component.constructor;
+            let handled;
+
+            if (ctor && ctor.getDerivedStateFromError !== null) {
+                component.setState(ctor.getDerivedStateFromError(error));
+                handled = component._dirty;
+            }
+
+            if (component.componentDidCatch !== null) {
+                component.componentDidCatch(error);
+                handled = component._dirty;
+            }
+
+            // これがエラー境界なので、回避されたことと中間ハイドレーションかどうかをマークする
+            if (handled) {
+                component._pendingError = component;
+                return component;
+            }
+        } catch (e) {
+            error = e;
+        }
+    }
+
+    throw error;
 };
-
-const actions: ActionSet<CounterState, { increment: {} }> = {
-    increment(state) {
-        console.log('state', state);
-        state.count++;
-    },
-};
-
-type CounterActions = typeof actions;
-
-const view: View<CounterState, CounterActions> = (state, actions) =>
-    h(
-        'div',
-        {},
-        h('p', {}, state.count),
-        h('button', { onClick: () => actions.increment(state, {}) }, 'Count up')
-    );
-
-const el = document.getElementById('root')!;
-const run = makeApp({ el, initialState, view, actions });
-run();
