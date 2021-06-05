d59ce39698a2d735b9f6d0671d968256c3f7ab76
0918nobita <nobita.0918@gmail.com>
2021-06-05 14:14:29 +0900

    :white_check_mark: describe を使ってテストをグルーピングした

--- src/diff/catchError.spec.ts
+++ src/diff/catchError.spec.ts
@@ -9,147 +9,153 @@ const createCommonConfig = () => ({
     options: createOptions(),
 });
 
-it('_catchError', () => {
-    const { env, options } = createCommonConfig();
-    const error = new Error();
-    const vnode: VNode<AnyObject> = {
-        type: 'div',
-        _component: null,
-        _parent: null,
-    };
-    expect(() => _catchError(env, options, error, vnode)).toThrowError();
-});
-
-it('_catchError (when componentDidCatch is defined)', () => {
-    const { env, options } = createCommonConfig();
-    const error = new Error();
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const componentDidCatch: jest.Mock<void, [Error]> = jest
-        .fn()
-        .mockName('componentDidCatch');
-    const component = new Component<EmptyObject, EmptyObject>({});
-    component.componentDidCatch = componentDidCatch;
-    const vnode: VNode<AnyObject> = {
-        type: 'button',
-        _component: null,
-        _parent: { type: 'div', _component: component, _parent: null },
-    };
-    expect(() => _catchError(env, options, error, vnode)).toThrowError();
-    expect(componentDidCatch).toBeCalledTimes(1);
-});
-
-it('_catchError (when vnode._parent._component is null)', () => {
-    const { env, options } = createCommonConfig();
-    const error = new Error();
-    const vnode: VNode<AnyObject> = {
-        type: 'button',
-        _component: null,
-        _parent: { type: 'div', _component: null, _parent: null },
-    };
-    expect(() => _catchError(env, options, error, vnode)).toThrowError();
-});
-
-it('_catchError (when vnode._parent._component._processingException is true)', () => {
-    const { env, options } = createCommonConfig();
-    const error = new Error();
-    const component = new Component<EmptyObject, EmptyObject>({});
-    component._processingException = true;
-    const vnode: VNode<AnyObject> = {
-        type: 'button',
-        _component: null,
-        _parent: { type: 'div', _component: component, _parent: null },
-    };
-    expect(() => _catchError(env, options, error, vnode)).toThrowError();
-});
-
-it('_catchError (when ctor.getDerivedStateFromError is defined)', () => {
-    const { env, options } = createCommonConfig();
-    const error = new Error();
-    const componentClass = Component;
-    const getDerivedStateFromError = jest
-        .fn()
-        .mockName('getDerivedStateFromError')
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        .mockImplementation((_err) => ({ hasError: true }));
-    componentClass.getDerivedStateFromError = getDerivedStateFromError;
-    const component = new componentClass<EmptyObject, { hasError: boolean }>(
-        {}
-    );
-    const setState = jest.fn().mockName('setState');
-    component.setState = setState;
-    const vnode: VNode<AnyObject> = {
-        type: 'button',
-        _component: null,
-        _parent: { type: 'div', _component: component, _parent: null },
-    };
-    expect(() => _catchError(env, options, error, vnode)).toThrowError();
-    expect(getDerivedStateFromError).toBeCalledTimes(1);
-    expect(setState).toBeCalledTimes(1);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    expect(setState.mock.calls[0][2]).toEqual({
-        hasError: true,
+describe('_catchError', () => {
+    it('when _parent is null', () => {
+        const { env, options } = createCommonConfig();
+        const error = new Error();
+        const vnode: VNode<AnyObject> = {
+            type: 'div',
+            _component: null,
+            _parent: null,
+        };
+        expect(() => _catchError(env, options, error, vnode)).toThrowError();
     });
-});
 
-it('_catchError (when ctor.getDerivedStateFromError is defined)', () => {
-    const { env, options } = createCommonConfig();
-    const error = new Error();
-    const componentClass = Component;
-    const getDerivedStateFromError = jest
-        .fn()
-        .mockName('getDerivedStateFromError')
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        .mockImplementation((_err) => ({ hasError: true }));
-    componentClass.getDerivedStateFromError = getDerivedStateFromError;
-    const component = new componentClass<EmptyObject, { hasError: boolean }>(
-        {}
-    );
-    const setState = jest.fn().mockName('setState');
-    component.setState = setState;
-    const vnode: VNode<AnyObject> = {
-        type: 'button',
-        _component: null,
-        _parent: { type: 'div', _component: component, _parent: null },
-    };
-    expect(() => _catchError(env, options, error, vnode)).toThrowError();
-    expect(getDerivedStateFromError).toBeCalledTimes(1);
-    expect(setState).toBeCalledTimes(1);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    expect(setState.mock.calls[0][2]).toEqual({
-        hasError: true,
+    it('when component.componentDidCatch is defined', () => {
+        const { env, options } = createCommonConfig();
+        const error = new Error();
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const componentDidCatch: jest.Mock<void, [Error]> = jest
+            .fn()
+            .mockName('componentDidCatch');
+        const component = new Component<EmptyObject, EmptyObject>({});
+        component.componentDidCatch = componentDidCatch;
+        const vnode: VNode<AnyObject> = {
+            type: 'button',
+            _component: null,
+            _parent: { type: 'div', _component: component, _parent: null },
+        };
+        expect(() => _catchError(env, options, error, vnode)).toThrowError();
+        expect(componentDidCatch).toBeCalledTimes(1);
     });
-});
 
-it('_catchError (when component.setState throws an exception)', () => {
-    const { env, options } = createCommonConfig();
-    const error1 = new Error('1');
-    const error2 = new Error('2');
-    const componentClass = Component;
-    componentClass.getDerivedStateFromError = jest.fn();
-    const component = new componentClass({});
-    component.setState = jest.fn().mockImplementation(() => {
-        throw error2;
+    it('when _parent._component is null', () => {
+        const { env, options } = createCommonConfig();
+        const error = new Error();
+        const vnode: VNode<AnyObject> = {
+            type: 'button',
+            _component: null,
+            _parent: { type: 'div', _component: null, _parent: null },
+        };
+        expect(() => _catchError(env, options, error, vnode)).toThrowError();
     });
-    const vnode: VNode<AnyObject> = {
-        type: 'button',
-        _component: null,
-        _parent: { type: 'div', _component: component, _parent: null },
-    };
-    expect(() => _catchError(env, options, error1, vnode)).toThrowError('2');
-});
 
-it('_cacthError (when component._dirty is true)', () => {
-    const { env, options } = createCommonConfig();
-    const error = new Error();
-    const componentClass = Component;
-    componentClass.getDerivedStateFromError = jest.fn();
-    const component = new componentClass({});
-    component._dirty = true;
-    const vnode: VNode<AnyObject> = {
-        type: 'button',
-        _component: null,
-        _parent: { type: 'div', _component: component, _parent: null },
-    };
-    expect(() => _catchError(env, options, error, vnode)).not.toThrow();
-    expect(component._pendingError).toBeTruthy();
+    it('when _parent._component._processingException is true', () => {
+        const { env, options } = createCommonConfig();
+        const error = new Error();
+        const component = new Component<EmptyObject, EmptyObject>({});
+        component._processingException = true;
+        const vnode: VNode<AnyObject> = {
+            type: 'button',
+            _component: null,
+            _parent: { type: 'div', _component: component, _parent: null },
+        };
+        expect(() => _catchError(env, options, error, vnode)).toThrowError();
+    });
+
+    it('when ctor.getDerivedStateFromError is defined', () => {
+        const { env, options } = createCommonConfig();
+        const error = new Error();
+        const componentClass = Component;
+        const getDerivedStateFromError = jest
+            .fn()
+            .mockName('getDerivedStateFromError')
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            .mockImplementation((_err) => ({ hasError: true }));
+        componentClass.getDerivedStateFromError = getDerivedStateFromError;
+        const component = new componentClass<
+            EmptyObject,
+            { hasError: boolean }
+        >({});
+        const setState = jest.fn().mockName('setState');
+        component.setState = setState;
+        const vnode: VNode<AnyObject> = {
+            type: 'button',
+            _component: null,
+            _parent: { type: 'div', _component: component, _parent: null },
+        };
+        expect(() => _catchError(env, options, error, vnode)).toThrowError();
+        expect(getDerivedStateFromError).toBeCalledTimes(1);
+        expect(setState).toBeCalledTimes(1);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        expect(setState.mock.calls[0][2]).toEqual({
+            hasError: true,
+        });
+    });
+
+    it('when ctor.getDerivedStateFromError is defined', () => {
+        const { env, options } = createCommonConfig();
+        const error = new Error();
+        const componentClass = Component;
+        const getDerivedStateFromError = jest
+            .fn()
+            .mockName('getDerivedStateFromError')
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            .mockImplementation((_err) => ({ hasError: true }));
+        componentClass.getDerivedStateFromError = getDerivedStateFromError;
+        const component = new componentClass<
+            EmptyObject,
+            { hasError: boolean }
+        >({});
+        const setState = jest.fn().mockName('setState');
+        component.setState = setState;
+        const vnode: VNode<AnyObject> = {
+            type: 'button',
+            _component: null,
+            _parent: { type: 'div', _component: component, _parent: null },
+        };
+        expect(() => _catchError(env, options, error, vnode)).toThrowError();
+        expect(getDerivedStateFromError).toBeCalledTimes(1);
+        expect(setState).toBeCalledTimes(1);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        expect(setState.mock.calls[0][2]).toEqual({
+            hasError: true,
+        });
+    });
+
+    it('when component.setState throws an exception', () => {
+        const { env, options } = createCommonConfig();
+        const error1 = new Error('1');
+        const error2 = new Error('2');
+        const componentClass = Component;
+        componentClass.getDerivedStateFromError = jest.fn();
+        const component = new componentClass({});
+        component.setState = jest.fn().mockImplementation(() => {
+            throw error2;
+        });
+        const vnode: VNode<AnyObject> = {
+            type: 'button',
+            _component: null,
+            _parent: { type: 'div', _component: component, _parent: null },
+        };
+        expect(() => _catchError(env, options, error1, vnode)).toThrowError(
+            '2'
+        );
+    });
+
+    it('when component._dirty is true', () => {
+        const { env, options } = createCommonConfig();
+        const error = new Error();
+        const componentClass = Component;
+        componentClass.getDerivedStateFromError = jest.fn();
+        const component = new componentClass({});
+        component._dirty = true;
+        const vnode: VNode<AnyObject> = {
+            type: 'button',
+            _component: null,
+            _parent: { type: 'div', _component: component, _parent: null },
+        };
+        expect(() => _catchError(env, options, error, vnode)).not.toThrow();
+        expect(component._pendingError).toBeTruthy();
+    });
 });
