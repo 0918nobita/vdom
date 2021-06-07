d1f7fdae862a1e9f3cadd929e30731b55d13b0b6
0918nobita <nobita.0918@gmail.com>
2021-06-07 15:25:54 +0900

    :art: Improve readability

--- scripts/formatCoverageReport.js
+++ scripts/formatCoverageReport.js
@@ -2,14 +2,25 @@ const fs = require('fs/promises');
 const path = require('path');
 
 (async () => {
+    const generateComment = (report) => {
+        const header = '## Jest Coverage Report\n\n';
+        const table =
+            '| File | Statements | Branches | Functions | Lines |\n' +
+            '| --- | --- | --- | --- | --- |\n' +
+            `| Total | ${report.total.statements.pct}%  | ${report.total.branches.pct}% | ${report.total.functions.pct}% | ${report.total.lines.pct}% |\n`;
+        const body = `${header}${table}`;
+        return { body };
+    };
+
     try {
-        const report = JSON.parse(
-            await fs.readFile(
-                path.join(__dirname, '../coverage/coverage-summary.json')
-            )
+        const reportFilePath = path.join(
+            __dirname,
+            '../coverage/coverage-summary.json'
         );
-        const body = `## Jest Coverage Report\n\n| File | Statements | Branches | Functions | Lines |\n| --- | --- | --- | --- | --- |\n| Total | ${report.total.statements.pct}%  | ${report.total.branches.pct}% | ${report.total.functions.pct}% | ${report.total.lines.pct}% |\n`;
-        await fs.writeFile('prComment.json', JSON.stringify({ body }));
+        const reportFileContent = await fs.readFile(reportFilePath);
+        const report = JSON.parse(reportFileContent);
+        const comment = JSON.stringify(generateComment(report));
+        await fs.writeFile('prComment.json', comment);
     } catch (e) {
         console.error('Failed to collect coverage');
         console.error(e);
